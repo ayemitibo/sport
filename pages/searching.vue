f0093326381e78a281aa18db7d393327d40516f7
Ayemitibo Abiodun <ayemitiboabiodun02@gmail.com>
2021-12-20 09:53:30 +0100

    chore: added notification

--- pages/searching.vue
+++ pages/searching.vue
@@ -6,7 +6,9 @@
 		>
 		</nav-image>
 		<div class="bg-gfinity-blue main">
-			<div class="bordered"></div>
+			<div class="bordered">
+				<img src="@/assets/images/arrow-left.svg" alt="" srcset="" />
+			</div>
 			<div class="ml-4">
 				<p class="main__title">Matchmaking Lobby</p>
 				<h1 class="main__header">Searching for compatible opponents</h1>
@@ -38,6 +40,38 @@
 				</section>
 			</div>
 		</div>
+		<div class="notifications">
+			<div class="toast-wrapper">
+				<span data-v-0c939f8a="" class="callout-small-bold w-2/3">
+					Searching...
+				</span>
+				<span class="flex justify-between items-center w-1/3"
+					><span class="callout-small">05:38</span>
+					<button>
+						<svg
+							width="14"
+							height="14"
+							viewBox="0 0 16 16"
+							fill="currentColor"
+							xmlns="http://www.w3.org/2000/svg"
+							class=""
+						>
+							<rect
+								x="2.34314"
+								y="0.727051"
+								width="18.2857"
+								height="2.28571"
+								transform="rotate(45 2.34314 0.727051)"
+							></rect>
+							<rect
+								width="18.2857"
+								height="2.28571"
+								transform="matrix(-0.707107 0.707107 0.707107 0.707107 13.6569 0.727051)"
+							></rect>
+						</svg></button
+				></span>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
@@ -58,7 +92,7 @@ export default {
 .bordered {
 	height: 105px;
 	width: 105px;
-	@apply bg-dark-grey;
+	@apply bg-dark-grey flex justify-center items-center;
 }
 
 .top-upper {
@@ -112,5 +146,25 @@ export default {
 .rect {
 	width: 12px;
 	height: 2px;
+}
+.toast-wrapper {
+	display: flex;
+	align-items: center;
+	color: white;
+	width: 294px;
+	height: 89px;
+	padding-right: 24px;
+	padding-left: 24px;
+	border-radius: 0;
+	--tw-bg-opacity: 1;
+	background-color: rgba(27, 27, 27, var(--tw-bg-opacity));
+}
+
+.notifications {
+	position: absolute;
+	bottom: -2.25rem;
+	right: 0px;
+	z-index: 50;
+	transform: translate(0px, 0px);
 }
 </style>
